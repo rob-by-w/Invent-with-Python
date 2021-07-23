4c182a99b779f6494859af8bc770d4744605cd4d
rob-by-w <rwayong.code@gmail.com>
2021-07-23 10:47:23 +0900

    Chapter 8 added save to text

--- Chapter_8_CalendarMaker/CalendarMaker.py
+++ Chapter_8_CalendarMaker/CalendarMaker.py
@@ -2,8 +2,55 @@ import datetime
 
 DAYS = ('Sunday', 'Monday', 'Tuesday', 'Wednesday',
         'Thursday', 'Friday', 'Saturday')
-BLANK_LINE = ''.join(['|' + ' '*10 for _ in DAYS]) + '|'
-WEEK_BREAK = ''.join(['+'+'-'*10 for _ in DAYS]) + '+'
+BLANK_LINE = ''.join(['|' + ' '*10 for _ in DAYS]) + '|\n'
+WEEK_BREAK = ''.join(['+'+'-'*10 for _ in DAYS]) + '+\n'
+
+
+def main():
+    print('Calendar Maker')
+    print('Enter the year for the calendar:')
+
+    while True:
+        year = input('> ')
+        if year.isdigit() and eval(year + '> 0'):
+            year = int(year)
+            break
+        print('Invalid input. Please enter a year')
+
+    print('Enter the month for the calendar, 1-12:')
+    while True:
+        month = input('> ')
+        if month.isdigit() and eval('1<=' + month + '<=12'):
+            month = int(month)
+            break
+        print('Invalid input. Please enter a month')
+
+    inputDate = datetime.date(year, month, 1)
+    endDate = add_month(inputDate, 1)
+    currentDate = datetime.date(year, month, 1)
+    while currentDate.weekday() != 6:
+        currentDate -= datetime.timedelta(1)
+
+    output = ''
+    output += (' '*33 + datetime.datetime.strftime(inputDate, '%B %Y') + '\n')
+    output += '...Sunday.....Monday....Tuesday...Wednesday...Thursday....Friday....Saturday..\n'
+    output += WEEK_BREAK
+    while currentDate < endDate:
+        for _ in DAYS:
+            output += ('|' + str(currentDate.day) + ' ' *
+                       (10-len(str(currentDate.day))))
+            currentDate += datetime.timedelta(1)
+        output += '|\n'
+
+        for _ in range(3):
+            output += BLANK_LINE
+        output += WEEK_BREAK
+
+    print(output)
+    outputText = f'calendar_{year}_{month}.txt'
+    with open(outputText, 'w') as f:
+        f.write(output)
+    print(f'Saved to {outputText}')
 
 
 def add_month(sourceDate, month):
@@ -13,42 +60,5 @@ def add_month(sourceDate, month):
     return datetime.date(newYear, newMonth, 1)
 
 
-print('Calendar Maker')
-print('Enter the year for the calendar:')
-
-while True:
-    year = input('> ')
-    if year.isdigit() and eval(year + '> 0'):
-        year = int(year)
-        break
-    print('Invalid input. Please enter a year')
-
-print('Enter the month for the calendar, 1-12:')
-while True:
-    month = input('> ')
-    if month.isdigit() and eval('1<=' + month + '<=12'):
-        month = int(month)
-        break
-    print('Invalid input. Please enter a month')
-
-inputDate = datetime.date(year, month, 1)
-endDate = add_month(inputDate, 1)
-currentDate = datetime.date(year, month, 1)
-while currentDate.weekday() != 6:
-    currentDate -= datetime.timedelta(1)
-
-print(' '*33 + datetime.datetime.strftime(inputDate, '%B %Y'))
-print('...Sunday.....Monday....Tuesday...Wednesday...Thursday....Friday....Saturday..')
-print(WEEK_BREAK)
-
-while currentDate < endDate:
-    for _ in DAYS:
-        print('|', end='')
-        print(str(currentDate.day) + ' '*(10-len(str(currentDate.day))), end='')
-
-        currentDate += datetime.timedelta(1)
-    print('|')
-
-    for _ in range(3):
-        print(BLANK_LINE)
-    print(WEEK_BREAK)
+if __name__ == '__main__':
+    main()
